eb4c54aa0a73464fef22c9d930a767b11478883b
Abzeede(Bank) <purijit@satang.com>
2018-06-25 23:27:20 +0700

    make doc more fancy

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { render } from "react-dom";
-import Flag from "../../lib";
-import "./styles.css";
+import React from "react"
+import { render } from "react-dom"
+import Flag from "../../lib"
+import "./styles.css"
 import countries from './countries'
 
 function Demo() {
   return (
     <div>
       <h1 className="title">rc-national-flag</h1>
-      <div>
+      <div className="container">
         {
           Object.keys(countries).map((code) => (
-            <Flag className="flag" width={50} code={code} />
+            <Flag className="flag" code={code} />
           ))
         }
       </div>
     </div>
-  );
+  )
 }
 
-render(<Demo />, document.getElementById("app"));
+render(<Demo />, document.getElementById("app"))
